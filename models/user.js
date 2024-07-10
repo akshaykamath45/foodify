105ddf724d78a745d0d59355ff053b4e5dc8ca88
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-07-11 00:29:59 +0530

    chore: minor changes

--- models/user.js
+++ models/user.js
@@ -1,47 +1,27 @@
 const mongoose = require("mongoose")
-const User = require("./user")
 
-const restaurantSchema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    cuisine: {
-        type: String
-    },
-    address: {
-        type: String
-    },
-    city: {
-        type: String
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5
-    },
-    menu: [
-        {
-            name: String,
-            price: Number,
-            description: String,
-            isVeg: Boolean
-        }
-    ],
-    averageRating: {
-        type: Number,
-        default: 0
-    },
-    reviews: [
-        {
-            user: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            },
-            text: String,
-            rating: Number
-        }
-    ]
-
+//user model
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phoneNumber: Number,
+  profilePictureUrl: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  nickname: String
+}, {
+  timestamps: true
 })
 
-module.exports = mongoose.model("Restaurant", restaurantSchema)
+const User = mongoose.model("User", userSchema)
+module.exports = User
